4105e0910da40387befa58eacbe64edaa2783767
Matej Radosovsky <59921647+Bartego@users.noreply.github.com>
2024-02-04 21:01:46 +0100

    moving home page to /image_upload

--- images/urls.py
+++ images/urls.py
@@ -3,7 +3,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index_view, name='images-index'),
+    path('image_upload', views.index_view, name='images-index'),
     path('upload/', views.upload_view, name='images-upload'),
     path('blog/', views.home, name='blog-home'),
     path('about/', views.about, name='blog-about'),
