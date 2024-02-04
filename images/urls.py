cdee27f01dd47e8a92f9bff2e5479e478797d90d
Matej Radosovsky <59921647+Bartego@users.noreply.github.com>
2024-02-04 21:11:27 +0100

    moving home page to /blog_home

--- images/urls.py
+++ images/urls.py
@@ -5,6 +5,6 @@ from . import views
 urlpatterns = [
     path('image_upload', views.index_view, name='images-index'),
     path('upload/', views.upload_view, name='images-upload'),
-    path('blog/', views.home, name='blog-home'),
+    path('', views.home, name='blog-home'),
     path('about/', views.about, name='blog-about'),
 ]
