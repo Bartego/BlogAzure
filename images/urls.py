e879f5dc9c298a59d82bdcbc6ab1ad6ca84dde03
Matej Radosovsky <59921647+Bartego@users.noreply.github.com>
2024-02-04 20:54:58 +0100

    upload url returned

--- images/urls.py
+++ images/urls.py
@@ -4,7 +4,7 @@ from . import views
 
 urlpatterns = [
     path('', views.index_view, name='images-index'),
-    # path('upload/', views.upload_view, name='images-upload'),
+    path('upload/', views.upload_view, name='images-upload'),
     path('blog/', views.home, name='blog-home'),
     path('about/', views.about, name='blog-about'),
 ]
