e551379632ce202652478edb1b0338af8473432e
Matej Radosovsky <59921647+Bartego@users.noreply.github.com>
2024-02-05 14:53:24 +0100

    added content into views.py in form of list,render list in home.html

--- images/views.py
+++ images/views.py
@@ -36,9 +36,27 @@ def upload_view(request):
 
     return redirect('images-index')
 
+posts = [
+    {
+        'author': 'CoreyMS',
+        'title': 'Blog Post 1',
+        'content': 'First post content',
+        'date_posted': 'August 27, 2018'
+    },
+    {
+        'author': 'Jane Doe',
+        'title': 'Blog Post 2',
+        'content': 'Second post content',
+        'date_posted': 'August 28, 2018'
+    }
+]
+
 
 def home(request):
-    return render(request, 'images/home.html')
+    context = {
+        'post': posts
+    }
+    return render(request, 'images/home.html', context)
 
 
 def about(request):
