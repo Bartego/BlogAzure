0de13de4ab0706778ec0dbb50408b758310304f0
Matej Radosovsky <59921647+Bartego@users.noreply.github.com>
2024-02-10 21:01:43 +0100

    added login and logout html files

--- core/urls.py
+++ core/urls.py
@@ -15,6 +15,7 @@ Including another URLconf
 """
 from django.conf.urls.static import static
 from django.contrib import admin
+from django.contrib.auth import views as auth_views
 from django.urls import path, include
 
 from core import settings
@@ -23,8 +24,10 @@ from users import views as user_views
 
 urlpatterns = [
     path('', include('images.urls')), # covers path('blog/', admin.site.urls) so its commented below
-    path('register/', user_views.register, name='register' ),
     path('admin/', admin.site.urls),
+    path('register/', user_views.register, name='register' ),
+    path('login/', auth_views.LoginView.as_view(template_name='users/login.html'), name='login' ),
+    path('logout/', auth_views.LogoutView.as_view(template_name='users/logout.html'), name='logout' ),
     # path('blog/', admin.site.urls),
 ]
 
