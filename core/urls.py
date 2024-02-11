0a19b7c869b80914b399822229b003e80e29daba
Matej Radosovsky <59921647+Bartego@users.noreply.github.com>
2024-02-11 20:54:32 +0100

    fixing users/views.py

--- core/urls.py
+++ core/urls.py
@@ -28,7 +28,7 @@ urlpatterns = [
     path('register/', user_views.register, name='register' ),
     path('login/', auth_views.LoginView.as_view(template_name='users/login.html'), name='login' ),
     path('logout/', auth_views.LogoutView.as_view(template_name='users/logout.html'), name='logout' ),
-    path('profile/', user_views.profile(template_name='users/profile.html'), name='profile' ),
+    path('profile/', user_views.profile, name='profile' ),
     # path('blog/', admin.site.urls),
 ]
 
