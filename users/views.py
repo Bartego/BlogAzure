ec94c724aa62443f49755a2500239a1e74c9978f
Matej Radosovsky <59921647+Bartego@users.noreply.github.com>
2024-02-13 09:58:46 +0100

    fixing typos in users/views.py

--- users/views.py
+++ users/views.py
@@ -15,7 +15,7 @@ def register(request):
             return redirect('login')
             # password = form.cleaned_data.get('passwrod')
     else:
-        form = UserRegisterForm
+        form = UserRegisterForm()
     return render(request, 'users/register.html', {'form': form})
 
 
@@ -28,8 +28,8 @@ def profile(request):
     if request.method == 'POST':
         u_form = UserUpdateForm(request.POST, instance=request.user)
         p_form = ProfileUpdateForm(request.POST,
-                                   request.Files,
-                                   instance=request.profile)
+                                   request.FILES,
+                                   instance=request.user.profile)
         if u_form.is_valid() and p_form.is_valid():
             u_form.save()
             p_form.save()
@@ -38,7 +38,7 @@ def profile(request):
   
     else:
         u_form = UserUpdateForm(instance=request.user)
-        p_form = ProfileUpdateForm(instance=request.profile)
+        p_form = ProfileUpdateForm(instance=request.user.profile)
     context = {
         'u_form': u_form,
         'p_form': p_form
