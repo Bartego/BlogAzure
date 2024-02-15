5860a360c9a525e722b3516c8ee6ef27a1bb4aad
Matej Radosovsky <59921647+Bartego@users.noreply.github.com>
2024-02-15 20:53:34 +0100

    trying copilot to fix my sht

--- users/models.py
+++ users/models.py
@@ -2,6 +2,7 @@ from django.db import models
 from django.contrib.auth.models import User
 from PIL import Image
 
+from django.core.files.storage import default_storage as storage
 
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
@@ -9,14 +10,34 @@ class Profile(models.Model):
 
     def __str__(self):
         return f'{self.user.username} Profile'
-    
-    def save(self):
-        super().save()
 
-        img = Image.open(self.image.path)
-        if img.height > 300 or img.width > 300:
-            output_size = (300,300)
-            img.thumbnail(output_size)
-            img.save(self.image.path)
+# # i need to fix following code as it doesnt work with azure blob storage
+        
+#     def save(self):
+#         super().save()
+
+#         img = Image.open(self.image.path)
+#         if img.height > 300 or img.width > 300:
+#             output_size = (300,300)
+#             img.thumbnail(output_size)
+#             img.save(self.image.path)
 
 # file = models.ImageField(upload_to='images/')
+
+    def save(self, *args, **kwargs):
+        super().save(*args, **kwargs)
+
+        if self.image:
+            # Open the image file using Django's storage API
+            f = storage.open(self.image.name, 'r')
+            image = Image.open(f)
+
+            if image.height > 300 or image.width > 300:
+                output_size = (300, 300)
+                image.thumbnail(output_size)
+                
+                # Save the resized image back to storage
+                f = storage.open(self.image.name, 'w')
+                image.save(f, format='JPEG')
+
+            f.close()   
