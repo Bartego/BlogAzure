eda0af471377e485d9dd47a862e80fbe5204f275
Matej Radosovsky <59921647+Bartego@users.noreply.github.com>
2024-02-16 20:28:16 +0100

    adding user_posts.html

--- images/urls.py
+++ images/urls.py
@@ -3,7 +3,8 @@ from .views import (PostListView,
                     PostDetailView,
                     PostCreateView,
                     PostUpdateView,
-                    PostDeleteView
+                    PostDeleteView,
+                    UserPostListView
 )
 
 from . import views
@@ -12,6 +13,7 @@ urlpatterns = [
    # path('image_upload', views.index_view, name='images-index'), #old project
    # path('upload/', views.upload_view, name='images-upload'), # old project
     path('', PostListView.as_view(), name='blog-home'),
+    path('user/<str:username>', UserPostListView.as_view(), name='user-posts'),
     path('post/<int:pk>/', PostDetailView.as_view(), name='post-detail'),
     path('post/<int:pk>/delete', PostDeleteView.as_view(), name='post-delete'),
     path('post/<int:pk>/update/', PostUpdateView.as_view(), name='post-update'),
